d0a3c80201e7abc3619254c4595eb5df78263df4
Sidcley Wilker <sidcleywilker@gmail.com>
2016-11-16 18:22:45 -0200

    Arquivo js comcluido

--- Sistema/002-implementacao/suap/systi/static/systi/js/acesso_biometrico.js
+++ Sistema/002-implementacao/suap/systi/static/systi/js/acesso_biometrico.js
@@ -1,8 +1,37 @@
 
 $(document).ready(function() {
 
-     document.getElementById('#id_tipo_do_usuario').style.display = 'none';
-     $("#id_id_usuario_fechadura").hide();
-     //$("#id_tipo_do_usuario").hide();
+    $("#id_tipo_do_usuario").change(function(){
+        show_hide_horas();
+    });
+
+    function dataFormatada() {
+      var data = new Date(),
+        dia = data.getDate(),
+        mes = data.getMonth() + 1,
+        ano = data.getFullYear();
+      return [dia, mes, ano].join('/');
+    }
+
+    $("#id_data_registro").val(dataFormatada);
+
+
+    $("#id_data_registro").attr('disabled', 'disabled');
+    $("#id_data_des_registro").attr('disabled', 'disabled');
+
+    var show_hide_horas = function() {
+
+        item = $('#id_tipo_do_usuario option:selected').text();
+
+        if(item === "Aluno") {
+            $(".field-servidor").hide(100);
+            $('.field-aluno').show();
+        } else {
+            $(".field-aluno").hide(100);
+            $(".field-servidor").show(100);
+        }
+    }
+
+show_hide_horas();
 
 });
